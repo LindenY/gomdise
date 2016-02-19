136bb9e72abdbc9c53b72a0792d0235d91952450
DesLin <desmond.zq.lin@gmail.com>
2016-02-19 00:14:14 -0500

    temp commit: change the pattern of how save parse works

--- parse.go
+++ parse.go
@@ -13,10 +13,6 @@ type parseState struct {
 
 
 func (pstate *parseState)pushAction(action *Action) {
-	if action == nil {
-		return
-	}
-
 	pstate.actions = append(pstate.actions, action)
 }
 
@@ -29,7 +25,6 @@ func (pstate *parseState)popAction() *Action {
 	pstate.actions = pstate.actions[0:len(pstate.actions)-1]
 	return ret
 }
-
 
 func parseSave(pstate *parseState, target interface{}) {
 	typ := reflect.TypeOf(target)
@@ -89,7 +84,6 @@ func parseMapSave(pstate *parseState, target interface{}) {
 }
 
 func parseStructSave(pstate *parseState, target interface{}) {
-
 }
 
 func parsePrimitiveSave(pstate *parseState, target interface{}) {
@@ -99,4 +93,126 @@ func parsePrimitiveSave(pstate *parseState, target interface{}) {
 
 	curr := pstate.actions[len(pstate.actions)-1]
 	curr.args = curr.args.Add(target)
+}
+
+
+// Save Parse Functions
+type parseFunc func(pstate *parseState, v reflect.Value)
+
+func saveParser(t reflect.Type) parseFunc {
+
+}
+
+func newSaveParser(t reflect.Type) {
+
+	switch t.Kind() {
+	case reflect.Bool, reflect.String, reflect.Float32, reflect.Float64,
+		reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return primitiveSaveParser
+
+	case reflect.Array, reflect.Slice:
+		return newArraySaveParser(t)
+
+	case reflect.Map:
+		return newMapSaveParser(t)
+
+	}
+}
+
+
+func primitiveSaveParser(pstate *parseState, v reflect.Value) {
+	if len(pstate.actions) == 0 {
+		panic(errors.New("Primitive type is not supported"))
+	}
+
+	curr := pstate.actions[len(pstate.actions)-1]
+	curr.args = curr.args.Add(v.Interface())
+}
+
+
+type arraySaveParser struct {
+	elemFunc parseFunc
+}
+
+func (asp *arraySaveParser) parse(pstate *parseState, v reflect.Value) {
+	arrKey := newKey(pstate.target)
+	prev := pstate.popAction()
+	curr := &Action{
+		name: "RPUSH",
+		args:redis.Args{arrKey},
+	}
+	pstate.pushAction(curr)
+
+	n := len(v)
+	for i := 0; i<n; i++ {
+		asp.elemFunc(pstate, v)
+	}
+
+	if prev != nil {
+		prev.args = prev.args.Add(arrKey)
+		pstate.pushAction(prev)
+	}
+}
+
+func newArraySaveParser(t reflect.Type) parseFunc {
+	asp := &arraySaveParser{saveParser(t.Elem())}
+	return asp.parse
+}
+
+
+type mapSaveParser struct {
+	elemFunc parseFunc
+}
+
+func (msp *mapSaveParser) parse(pstate *parseState, v reflect.Value) {
+	mapKey := newKey(pstate.target)
+	prev := pstate.popAction()
+	curr := &Action{
+		name:"HSET",
+		args:redis.Args{mapKey},
+	}
+	pstate.pushAction(curr)
+
+	skeys := v.MapKeys()
+	for _, skey := range skeys {
+		curr.args = curr.args.Add(skey)
+		msp.elemFunc(pstate, v)
+	}
+
+	if prev != nil {
+		prev.args = prev.args.Add(mapKey)
+		pstate.pushAction(prev)
+	}
+}
+
+func newMapSaveParser(t reflect.Type) parseFunc {
+	if t.Key().Kind() != reflect.String {
+		return unsupportedTypeParser
+	}
+
+	msp := &mapSaveParser{saveParser(t.Elem())}
+	return msp.parse
+}
+
+
+type structSaveParser struct {
+
+}
+
+func (ssp *structSaveParser) parse(pstate *parseState, v reflect.Value) {
+
+}
+
+func newStructSaveParser(t reflect.Type) parseFunc {
+	return nil
+}
+
+
+func unsupportedTypeParser(pstate *parseState, v reflect.Value) {
+	panic(&UnsupportedTypeError{v.Type()})
+}
+
+type UnsupportedTypeError struct {
+	Type reflect.Type
 }
