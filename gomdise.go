26531b5ad5a0134b15a64a60a23afe43ba533989
deslin <desmond.zq.lin@gmail.com>
2016-03-11 13:48:04 -0500

    Add gomdise library interface and its test file. Fix bug: pointerFindTemplate engrave args

--- gomdise.go
+++ gomdise.go
@@ -1,16 +1,19 @@
 package gomdies
 
 import (
-	"github.com/garyburd/redigo/redis"
 	"sync"
 	"runtime"
 	"errors"
 	"reflect"
+	"github.com/garyburd/redigo/redis"
+	"github.com/LindenY/gomdise/tpl"
+	"github.com/LindenY/gomdise/trans"
+	"github.com/LindenY/gomdise/mdl"
 )
 
 type Gomdise struct {
 	sync.RWMutex
-	pool redis.Pool
+	pool *redis.Pool
 	opts map[string]interface{}
 }
 
@@ -27,50 +30,56 @@ func (gom *Gomdise) GetOption(key string) interface{} {
 }
 
 
-func (gom *Gomdise) Save(args interface{}) (err error) {
+func (gom *Gomdise) Save(arg interface{}) (key string, err error) {
 	defer func() {
-		if r := recover(); r != nil {
-			if _, ok := r.(runtime.Error); ok {
-				panic(r)
-			}
-			if s, ok := r.(string); ok {
-				err = errors.New(s)
-			}
-			err = r.(error)
+		err = errorRecover()
+		if err != nil {
+			key = ""
 		}
 	}()
 
-
-	return nil
+	tpl := tpl.TCSave.GetTemplate(reflect.TypeOf(arg))
+	tran := trans.NewTransaction(gom.pool)
+	v := reflect.ValueOf(arg)
+	key = mdl.NewKey(v)
+	tpl.Engrave(&tran.Actions, key, v)
+	tran.Exec()
+	return key, nil
 }
 
 func (gom *Gomdise) Find(key string, dest interface{}) (err error) {
 	defer func() {
-		if r := recover(); r != nil {
-			if _, ok := r.(runtime.Error); ok {
-				panic(r)
-			}
-			if s, ok := r.(string); ok {
-				err = errors.New(s)
-			}
-			err = r.(error)
-		}
+		err = errorRecover()
 	}()
 
-	tpl := findTemplateForType(reflect.TypeOf(dest))
-	root := tpl.engrave(key)
-	tran := NewTransaction(gom.pool)
-	tran.Actions = append(tran.Actions, root)
+	tpl := tpl.TCFind.GetTemplate(reflect.TypeOf(dest))
+	tran := trans.NewTransaction(gom.pool)
+	tpl.Engrave(&tran.Actions, key)
+	root := tran.Actions[0]
 	tran.Exec()
-	Decode(root, dest)
+	mdl.Decode(root, dest)
 	return nil
 }
 
 
-func Init(pool redis.Pool) *Gomdise {
+func New(pool *redis.Pool) *Gomdise {
 	gom := &Gomdise{
 		pool:pool,
 		opts:make(map[string]interface{}, 0),
 	}
 	return gom
+}
+
+
+func errorRecover() (err error) {
+	if r := recover(); r != nil {
+		if _, ok := r.(runtime.Error); ok {
+			panic(r)
+		}
+		if s, ok := r.(string); ok {
+			err = errors.New(s)
+		}
+		err = r.(error)
+	}
+	return nil
 }
